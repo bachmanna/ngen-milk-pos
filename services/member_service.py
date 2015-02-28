301ff17b75303c451b39859414e04cc674cb7fd4
cackharot <cackharot@gmail.com>
2015-02-28 13:52:19 +0530

    added member save

--- services/member_service.py
+++ services/member_service.py
@@ -6,14 +6,24 @@ class MemberService:
     def __init__(self):
         pass
 
-    def add(self, name, cattle_type, mobile):
-        member = Member(name=name, cattle_type=cattle_type, mobile=mobile, status=True)
+    def add(self, name, cattle_type, mobile, _id=None):
+        member = Member(id=_id, name=name, cattle_type=cattle_type, mobile=mobile, status=True)
         commit()
         return member.id
 
     def get(self, _id):
         member = Member[_id]
         return member
+
+    def update(self, _id, name, cattle_type, mobile):
+        member = self.get(_id)
+        if not member:
+            return False
+        member.name = name
+        member.mobile = mobile
+        member.cattle_type = cattle_type
+        commit()
+        return True
 
     def search(self, name=None, mobile=None, cattle_type=None):
         query = select(p for p in Member)
@@ -23,6 +33,7 @@ class MemberService:
             query = query.filter(lambda x: x.mobile == mobile)
         if cattle_type:
             query = query.filter(lambda x: x.cattle_type == cattle_type)
+        query = query.order_by(Member.created_at.desc())
         lst = query[:]
         return lst
 
--- web/__init__.py
+++ web/__init__.py
@@ -5,6 +5,7 @@ from pony.orm import sql_debug, db_session
 from datetime import datetime
 
 from services.user_service import UserService
+from services.member_service import MemberService
 
 app = Flask(__name__, instance_relative_config=False)
 app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
@@ -121,10 +122,34 @@ def system_setup():
   return render_template("system_setup.jinja2")
 
 
-@app.route("/member")
+@app.route("/member", methods=['GET', 'POST'])
 @login_required
-def member():
-  return render_template("member.jinja2")
+def manager_member():
+  member_service = MemberService()
+  if request.method == 'POST':
+    code = request.form.get("code")
+    name = request.form.get("name")
+    mobile = request.form.get("mobile")
+    cattle_type = request.form.get("cattle_type")
+    if code and name and int(code):
+      id = int(code)
+      m = member_service.get(id)
+      if not m:
+        member_service.add(name, cattle_type, mobile, _id=id)
+      else:
+        member_service.update(id, name, cattle_type, mobile)
+    else:
+      flash("Invalid member data.")
+  member_list = member_service.search()
+  free_list = []
+  used_list = [int(x.id) for x in member_list]
+  for i in range(1,100):
+    if len(free_list) > 10:
+        break
+    if i not in used_list:
+      free_list.append(str(i))
+
+  return render_template("member.jinja2",member_list=member_list, free_list=free_list)
 
 
 @app.route("/data_reset")
