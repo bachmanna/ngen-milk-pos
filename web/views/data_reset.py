1c0268cf25f73ea53687015a402177379f01a79d
cackharot <cackharot@gmail.com>
2015-03-07 13:54:30 +0530

    added data backup and restore to csv feature

--- web/views/data_reset.py
+++ web/views/data_reset.py
@@ -32,9 +32,58 @@ def factory_reset():
 	flash(str(lazy_gettext(msg)))
 	return redirect(url_for("data_reset"))
 
+
 @app.route("/data_backup")
 @login_required
 def data_backup():
+	do_backup()
 	msg = "Data backup successfull!"
 	flash(str(lazy_gettext(msg)))
 	return redirect(url_for("data_reset"))
+
+
+@app.route("/data_restore")
+@login_required
+def data_restore():
+	do_restore()
+	msg = "Data restore successfull!"
+	flash(str(lazy_gettext(msg)))
+	return redirect(url_for("data_reset"))
+
+
+def do_backup():
+	import csv
+	from db_manager import db
+	from models.member import Member
+	import os
+	for table in db.metadata.tables.values():
+		filename = 'backup/%s.csv' % (table.name)
+		with open(os.path.join(app.root_path, filename), 'wb') as outfile:
+			outcsv = csv.writer(outfile)
+			outcsv.writerow([column.name for column in table.columns])
+			records = db.session.query(table).all()
+			[outcsv.writerow([getattr(curr, column.name) for column in table.columns]) for curr in records]
+			outfile.close()
+
+
+def do_restore():
+	import csv
+	from db_manager import db
+	from models.member import Member
+	import os
+	db.drop_all()
+	db.create_all()
+	for table in db.metadata.tables.values():
+		filename = 'backup/%s.csv' % (table.name)
+		fpath = os.path.join(app.root_path, filename)
+
+		if not os.path.isfile(fpath):
+			continue
+		print "Restore %s " % (filename)
+		with open(fpath, 'rb') as outfile:
+			cf = csv.DictReader(outfile, delimiter=',')
+			for row in cf:
+				smt = table.insert().values(**row)
+				db.engine.execute(smt)
+		print "Done"
+	db.session.commit()
