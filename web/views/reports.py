ece35d9791c3f5fc0fd71357f7366ebadb9dcb44
cackharot <cackharot@gmail.com>
2015-03-08 13:42:55 +0530

    updated report search UI and added absent list report

--- web/views/reports.py
+++ web/views/reports.py
@@ -1,6 +1,7 @@
 from flask import render_template, request, redirect, g, flash
 from flask_login import login_required
 from datetime import datetime
+from flask.ext.babel import lazy_gettext, gettext
 
 from web import app
 
@@ -20,7 +21,41 @@ def reports():
 def report_member_list():
   member_service = MemberService()
   member_list = member_service.search()
-  return render_template("member_list.jinja2",member_list=member_list)
+  cow_member_list = [x for x in member_list if x.cattle_type == "COW"]
+  buffalo_member_list = [x for x in member_list if x.cattle_type == "BUFFALO"]
+  return render_template("member_list.jinja2",member_list=member_list,cow_member_list=cow_member_list,buffalo_member_list=buffalo_member_list)
+
+
+@app.route("/absence_list")
+@login_required
+def absence_list():
+  shift = request.args.get("shift", "MORNING")
+  today = datetime.now().date()
+  day   = int(request.args.get("day", today.day))
+  month = int(request.args.get("month", today.month))
+  year  = int(request.args.get("year", today.year))
+  search_date = datetime(year, month, day).date()
+
+  if search_date > today:
+    search_date = today
+    flash(str(lazy_gettext("Date cannot be greater than today!")), "error")
+
+  collectionService = MilkCollectionService()
+  lst = collectionService.search(created_at=search_date, shift=shift)
+
+  member_service = MemberService()
+  member_list = member_service.search()
+
+  absence_members = []
+  mids = [x.member_id for x in lst]
+  for x in member_list:
+    if not x.id in mids:
+      absence_members.append(x)
+
+  cow_member_list = [x for x in absence_members if x.cattle_type == "COW"]
+  buffalo_member_list = [x for x in absence_members if x.cattle_type == "BUFFALO"]
+  return render_template("absence_list.jinja2",member_list=absence_members,cow_member_list=cow_member_list,buffalo_member_list=buffalo_member_list,search_date=search_date,shift=shift)
+
 
 @app.route("/detail_shift")
 @login_required
@@ -36,6 +71,7 @@ def report_detail_shift():
 
   return render_template("detail_shift.jinja2", mcollection=mcollection, member_list=members, summary=summary, search_date=search_date, shift=shift)
 
+
 @app.route("/shift_summary")
 @login_required
 def report_shift_summary():
@@ -48,3 +84,9 @@ def report_shift_summary():
   collectionService = MilkCollectionService()
   members, mcollection, summary = collectionService.get_milk_collection_and_summary(shift, search_date)
   return render_template("shift_summary.jinja2", mcollection=mcollection, member_list=members, summary=summary, search_date=search_date, shift=shift)
+
+
+@app.route("/payment_report")
+@login_required
+def payment_report():
+  return render_template("payment_report.jinja2")
