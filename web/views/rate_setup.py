066f91f698c2ac4c360aa0c2daacaa9ccd6b0a48
cackharot <cackharot@gmail.com>
2015-03-30 16:01:44 +0530

    updated rate setup layouts and added member import export

--- web/views/rate_setup.py
+++ web/views/rate_setup.py
@@ -31,11 +31,19 @@ def rate_fat():
 
   if request.method == 'POST':
     id = int(request.form.get("id", 0))
-    min_value = float(request.form.get("min_value", 0.0))
-    max_value = float(request.form.get("max_value", 0.0))
-    rate = float(request.form.get("rate", 0.0))
     cattle_type = request.form.get("cattle_type", "COW")
-    rate_service.update_fat_collection_rate(cattle_type,id,min_value,max_value,rate)
+    min_value = request.form.get("min_value", "0.0")
+    min_value = request.form.get("min_value", "0.0")
+    max_value = request.form.get("max_value", "0.0")
+    rate = request.form.get("rate", "0.0")
+    if min_value and max_value and rate:
+      min_value = float(min_value)
+      max_value = float(max_value)
+      rate = float(rate)
+      rate_service.update_fat_collection_rate(cattle_type,id,min_value,max_value,rate)
+      flash("Rate data saved successfully!")
+    else:
+      flash("Invalid data!", category="error")
 
   rate_list = rate_service.get_fat_collection_rate(cattle_type=cattle_type)
   return render_template("rate_fat.jinja2", cattle_type=cattle_type, rate_list=rate_list,rate_type="fat")
@@ -48,11 +56,16 @@ def rate_fat_and_snf():
   rate_service = RateService()
 
   if request.method == 'POST':
-    fat = float(request.form.get("fat", 0.0))
-    snf = float(request.form.get("snf", 0.0))
-    rate = float(request.form.get("rate", 0.0))
-    cattle_type = request.form.get("cattle_type", "COW")
-    rate_service.save_fat_and_snf_collection_rate(cattle_type=cattle_type,fat_value=fat,snf_value=snf,rate=rate)
+    try:
+      fat = float(request.form.get("fat", 0.0))
+      snf = float(request.form.get("snf", 0.0))
+      rate = float(request.form.get("rate", 0.0))
+      cattle_type = request.form.get("cattle_type", "COW")
+      rate_service.save_fat_and_snf_collection_rate(cattle_type=cattle_type,fat_value=fat,snf_value=snf,rate=rate)
+      flash("Rate data saved successfully!")
+    except Exception as e:
+      print e
+      flash("Invalid data!", category="error")
 
   rate_list = rate_service.get_fat_and_snf_collection_rate(cattle_type=cattle_type)
 
@@ -62,7 +75,7 @@ def rate_fat_and_snf():
     snf_list.add(x.snf_value)
     if not x.fat_value in rates.keys():
       rates[x.fat_value] = {}
-    rates[x.fat_value][x.snf_value] = fmtDecimal(x.rate)
+    rates[x.fat_value][x.snf_value] = x.rate
   snf_list = sorted(snf_list)
   return render_template("rate_fat_and_snf.jinja2", cattle_type=cattle_type, rate_list=rate_list, snf_list=snf_list, rates=rates, rate_type="fat_and_snf")
 
@@ -74,16 +87,21 @@ def rate_total_solid():
   rate_service = RateService()
 
   if request.method == 'POST':
-    data = {}
-    id = int(request.form.get("id", 0))
-    cattle_type = request.form.get("cattle_type", cattle_type)
-    data["min_fat"] = float(request.form.get("min_fat", 0.0))
-    data["max_fat"] = float(request.form.get("max_fat", 0.0))
-    data["fat_rate"] = float(request.form.get("fat_rate", 0.0))
-    data["min_snf"] = float(request.form.get("min_snf", 0.0))
-    data["max_snf"] = float(request.form.get("max_snf", 0.0))
-    data["snf_rate"] = float(request.form.get("snf_rate", 0.0))
-    rate_service.save_ts1_collection_rate(id=id, cattle_type=cattle_type, data=data)
+    try:
+      data = {}
+      id = int(request.form.get("id", 0))
+      cattle_type = request.form.get("cattle_type", cattle_type)
+      data["min_fat"] = float(request.form.get("min_fat", 0.0))
+      data["max_fat"] = float(request.form.get("max_fat", 0.0))
+      data["fat_rate"] = float(request.form.get("fat_rate", 0.0))
+      data["min_snf"] = float(request.form.get("min_snf", 0.0))
+      data["max_snf"] = float(request.form.get("max_snf", 0.0))
+      data["snf_rate"] = float(request.form.get("snf_rate", 0.0))
+      rate_service.save_ts1_collection_rate(id=id, cattle_type=cattle_type, data=data)
+      flash("Rate data saved successfully!")
+    except Exception as e:
+      print e
+      flash("Invalid data!", category="error")
 
   rate_list = rate_service.get_ts1_collection_rate(cattle_type=cattle_type)
   return render_template("rate_total_solid.jinja2", cattle_type=cattle_type, rate_list=rate_list,rate_type="ts1")
@@ -96,12 +114,17 @@ def rate_total_solid1():
   rate_service = RateService()
 
   if request.method == 'POST':
-    id = int(request.form.get("id", 0))
-    cattle_type = request.form.get("cattle_type", cattle_type)
-    min_value = float(request.form.get("min_value", 0.0))
-    max_value = float(request.form.get("max_value", 0.0))
-    rate = float(request.form.get("rate", 0.0))
-    rate_service.save_ts2_collection_rate(cattle_type,id,min_value,max_value,rate)
+    try:
+      id = int(request.form.get("id", 0))
+      cattle_type = request.form.get("cattle_type", cattle_type)
+      min_value = float(request.form.get("min_value", 0.0))
+      max_value = float(request.form.get("max_value", 0.0))
+      rate = float(request.form.get("rate", 0.0))
+      rate_service.save_ts2_collection_rate(cattle_type,id,min_value,max_value,rate)
+      flash("Rate data saved successfully!")
+    except Exception as e:
+      print e
+      flash("Invalid data!", category="error")
 
   rate_list = rate_service.get_ts2_collection_rate(cattle_type=cattle_type)
   return render_template("rate_total_solid1.jinja2", cattle_type=cattle_type, rate_list=rate_list,rate_type="ts2")
--- web/views/system_setup.py
+++ web/views/system_setup.py
@@ -1,7 +1,13 @@
 from flask import render_template, request, redirect, g, flash, url_for
 from flask_login import login_required
+from flask.ext.babel import lazy_gettext, gettext
+from dateutil import parser
+from datetime import datetime
+import os
+import sys
+import csv
 
-from web import app
+from web import app, get_backup_directory
 
 from services.member_service import MemberService
 from services.rate_service import RateService
@@ -32,20 +38,24 @@ def system_language():
 @login_required
 def manager_member():
   member_service = MemberService()
+  member = None
   if request.method == 'POST':
     code = request.form.get("code")
     name = request.form.get("name")
     mobile = request.form.get("mobile")
     cattle_type = request.form.get("cattle_type")
-    if code and name and int(code):
+    if code and int(code):
       id = int(code)
-      m = member_service.get(id)
-      if not m:
-        member_service.add(name, cattle_type, mobile, _id=id)
-      else:
-        member_service.update(id, name, cattle_type, mobile)
+      member = member_service.get(id)
+      if name and len(name) > 3:
+        if not member:
+          member_service.add(name, cattle_type, mobile, _id=id)
+        else:
+          member_service.update(id, name, cattle_type, mobile)
+        member = None
+        flash("Member saved successfully!")      
     else:
-      flash("Invalid member data.")
+      flash("Invalid member data.",category="error")
   member_list = member_service.search()
   free_list = []
   used_list = [int(x.id) for x in member_list]
@@ -55,4 +65,70 @@ def manager_member():
     if i not in used_list:
       free_list.append(str(i))
 
-  return render_template("member.jinja2",member_list=member_list, free_list=free_list)
+  if not member:
+    member = {}
+  return render_template("member.jinja2",member_list=member_list, free_list=free_list, member=member)
+
+
+@app.route("/member_export")
+@login_required
+def member_export():
+  if do_export():
+    flash(str(lazy_gettext("Export successfull!")))
+  else:
+    flash(str(lazy_gettext("Import failed!")), "error")
+  return redirect("/member")
+
+
+@app.route("/member_import")
+@login_required
+def member_import():
+  if do_import():
+    flash(str(lazy_gettext("Import successfull!")))
+  else:
+    flash(str(lazy_gettext("Import failed!")), "error")
+  return redirect("/member")
+
+
+def do_export():
+  directory = get_backup_directory()
+  table = Member.__table__
+  filename = 'member.csv'
+  fpath = os.path.join(directory, filename)
+  print "Backup to folder %s" % (fpath)
+  with open(fpath, 'wb') as outfile:
+      outcsv = csv.writer(outfile)
+      outcsv.writerow([column.name for column in table.columns])
+      records = db.session.query(table).all()
+      [outcsv.writerow([getattr(curr, column.name) for column in table.columns]) for curr in records]
+      outfile.close()
+  return True
+
+
+def do_import():
+  directory = get_backup_directory()
+  table = Member.__table__
+  filename = 'member.csv'
+  fpath = os.path.join(directory, filename)
+  print "Import from folder %s" % (fpath)
+  if not os.path.isfile(fpath):
+      return False
+  with open(fpath, 'rb') as infile:
+      # delete all rows
+      db.engine.execute(table.delete())
+      cf = csv.DictReader(infile, delimiter=',')
+      smt = table.insert()
+      data = [row for row in cf]
+      if len(data) > 0:
+        if "created_at" in row.keys():
+          for row in data:
+            row["created_at"] = parser.parse(row["created_at"])
+            if len(row["updated_at"]) > 0:
+              row["updated_at"] = parser.parse(row["updated_at"])
+            else:
+              row["updated_at"] = None
+              row["updated_by"] = None
+            row["status"] = row["status"] == "True"
+        db.engine.execute(smt, data)
+      infile.close()
+  return True
