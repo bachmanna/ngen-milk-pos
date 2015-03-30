06f98c7331aa3183f3ab206bf1aabc3c88cff0cd
cackharot <cackharot@gmail.com>
2015-03-30 18:38:46 +0530

    updated collection page to use total milk graph not cow/buffalo separately

--- web/views/collections.py
+++ web/views/collections.py
@@ -45,32 +45,36 @@ def collection():
 
   if request.method == "POST":
     entity = {}
-    mcode = int(request.form.get("member_code"))
-    collection_id = request.form.get("collection_id", None)
-    entity["member"] = member_service.get(mcode)
-    entity["shift"] = request.form.get("shift", shift)
-    entity["fat"] = float(request.form.get("fat"))
-    entity["snf"] = float(request.form.get("snf"))
-    entity["clr"] = float(request.form.get("clr"))
-    entity["aw"] = float(request.form.get("water"))
-    entity["qty"] = float(request.form.get("qty"))
-    entity["rate"] = float(request.form.get("rate"))
-    entity["total"] = float(request.form.get("total"))
-    entity["created_by"] = current_user.id
-    entity["created_at"] = parser.parse(request.form.get("created_at", str(date)))
-    entity["status"] = True
-    print entity
-    if collection_id and int(collection_id) > 0:
-      collectionService.update(int(collection_id), entity)
+    member_code = request.form.get("member_code")
+    if member_code and len(member_code) > 0 and member_code.isdigit():
+      mcode = int(member_code)
+      collection_id = request.form.get("collection_id", None)
+      entity["member"] = member_service.get(mcode)
+      entity["shift"] = request.form.get("shift", shift)
+      entity["fat"] = float(request.form.get("fat"))
+      entity["snf"] = float(request.form.get("snf"))
+      entity["clr"] = float(request.form.get("clr"))
+      entity["aw"] = float(request.form.get("water"))
+      entity["qty"] = float(request.form.get("qty"))
+      entity["rate"] = float(request.form.get("rate"))
+      entity["total"] = float(request.form.get("total"))
+      entity["created_by"] = current_user.id
+      entity["created_at"] = parser.parse(request.form.get("created_at", str(date)))
+      entity["status"] = True
+      print entity
+      if collection_id and int(collection_id) > 0:
+        collectionService.update(int(collection_id), entity)
+      else:
+        collectionService.add(entity)
+
+      try:
+        printTicket(entity)
+      except Exception as e:
+        print e
+
+      flash("Saved successfully!", "success")
     else:
-      collectionService.add(entity)
-
-    try:
-      printTicket(entity)
-    except Exception as e:
-      print e
-
-    flash("Saved successfully!", "success")
+      flash("Invalid data!", "error")
     return redirect("/collection")
   
   member_list = member_service.search()
@@ -79,10 +83,10 @@ def collection():
   collection_members, mcollection, summary = collectionService.get_milk_collection_and_summary(shift, date.date())
 
   qty_total = 38.0
-  c_qty = summary["milk"][0]%qty_total
-  b_qty = summary["milk"][1]%qty_total
-  c_qty = (1 - ((qty_total - c_qty)/qty_total))*100.0
-  b_qty = (1 - ((qty_total - b_qty)/qty_total))*100.0
+  t_qty = summary["milk"][0] + summary["milk"][1]
+  t_qty = (1 - ((qty_total - t_qty)/qty_total))*100.0
+  
+  currency_symbol = get_currency_symbol()
 
   return render_template('collection.jinja2',
     shift=shift,
@@ -91,8 +95,8 @@ def collection():
     date=date,
     collection_members=collection_members,
     summary=summary,
-    c_qty=c_qty,
-    b_qty=b_qty)
+    t_qty=t_qty,
+    currency_symbol=currency_symbol)
 
 
 @app.route("/get_collection_data")
