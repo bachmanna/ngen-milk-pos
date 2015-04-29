98ed03abcada27351d09d983fca569c4ff6cce89
cackharot <cackharot@gmail.com>
2015-04-29 19:55:44 +0530

    for tagging 1.0

--- mpos/_version.py
+++ mpos/_version.py
@@ -1,4 +1,4 @@
 
 # This file is originally generated from Git information by running 'setup.py
 # version'. Distribution tarballs contain a pre-generated copy of this file.
-__version__ = '1.0.0-dirty'
+__version__ = '1.0.0'
