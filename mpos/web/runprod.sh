4670792f9a2aa44ed0b4c85256c5b336ca93ff57
cackharot <cackharot@gmail.com>
2015-09-18 21:20:04 +0530

    updated ticket spacing and wlan config

--- mpos/web/runprod.sh
+++ mpos/web/runprod.sh
@@ -1,2 +1,10 @@
-#!/bin/sh
-sudo kill -9 `cat /tmp/mpos-master.pid` && sudo uwsgi --ini /home/pi/mpos/web/uwsgi.ini
+#!/bin/bash
+PID='/tmp/mpos-master.pid'
+UWSGI='/home/pi/mpos/web/uwsgi.ini'
+
+if [ -f "$PID" ]; then
+  pid=`cat $PID`
+  (sudo kill -9 "$pid"; sudo uwsgi --ini "$UWSGI")
+else
+  sudo uwsgi --ini "$UWSGI"
+fi
