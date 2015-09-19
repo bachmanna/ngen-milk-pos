5e7f52da699d61b1e441f8fbc0b2e9af4b8db687
cackharot <cackharot@gmail.com>
2015-09-19 16:39:28 +0530

    Fixed javascript time to prepend '0' to minutes and updated version to 1.0.11

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-(python setup.py bdist_wheel;cp dist/mpos-1.0.0-py2-none-any.whl dist/mpos-1.0.0.zip;)
+# (python setup.py bdist_wheel;cp dist/mpos-1.0.0-py2-none-any.whl dist/mpos-1.0.0.zip;)
+python setup.py bdist_wheel
